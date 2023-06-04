62f21c2b6b3d218d9a30257afbecb367b879dc17
Ileen Fan <ifan2011@gmail.com>
2023-06-04 00:52:57 -0700

    added endpoints for food nutrients

--- routes/foods.py
+++ routes/foods.py
@@ -26,3 +26,75 @@ def calories():
     ).limit(limit)
     results = list(cursor)
     return jsonify(results)
+
+@foods.route('/protein', methods=['GET'])
+def protein():
+    """
+    Returns a list of foods with grams of protein in a given range.
+    """
+
+    min = request.args.get('min', default=0, type=int)
+    max = request.args.get('max', default=88, type=int)
+    limit = request.args.get('limit', default=5, type=int)
+    range = {"$gte": min, "$lte": max} if max else {"$gte": min}
+    cursor = db.foods.find(
+        {"Data.Protein": range},
+        {"_id": 0}
+    ).limit(limit)
+    results = list(cursor)
+
+    return jsonify(results)
+
+@foods.route('/carbs', methods=['GET'])
+def carbs():
+    """
+    Returns a list of foods with grams of protein in a given range.
+    """
+
+    min = request.args.get('min', default=0, type=int)
+    max = request.args.get('max', default=100, type=int)
+    limit = request.args.get('limit', default=5, type=int)
+    range = {"$gte": min, "$lte": max} if max else {"$gte": min}
+    cursor = db.foods.find(
+        {"Data.Carbohydrate": range},
+        {"_id": 0}
+    ).limit(limit)
+    results = list(cursor)
+
+    return jsonify(results)
+
+@foods.route('/fiber', methods=['GET'])
+def fiber():
+    """
+    Returns a list of foods with grams of protein in a given range.
+    """
+
+    min = request.args.get('min', default=0, type=int)
+    max = request.args.get('max', default=100, type=int)
+    limit = request.args.get('limit', default=5, type=int)
+    range = {"$gte": min, "$lte": max} if max else {"$gte": min}
+    cursor = db.foods.find(
+        {"Data.Fiber": range},
+        {"_id": 0}
+    ).limit(limit)
+    results = list(cursor)
+
+    return jsonify(results)
+
+@foods.route('/fat', methods=['GET'])
+def fat():
+    """
+    Returns a list of foods with grams of protein in a given range.
+    """
+
+    min = request.args.get('min', default=0, type=int)
+    max = request.args.get('max', default=100, type=int)
+    limit = request.args.get('limit', default=5, type=int)
+    range = {"$gte": min, "$lte": max} if max else {"$gte": min}
+    cursor = db.foods.find(
+        {"Data.Fat.Total Lipid": range},
+        {"_id": 0}
+    ).limit(limit)
+    results = list(cursor)
+
+    return jsonify(results)
